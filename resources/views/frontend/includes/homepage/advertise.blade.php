fe2d2272fc18d40ef1cfc3d8b5a51a263d5b3149
subhu2010 <maharjansuzan@gmail.com>
2022-04-13 11:54:58 +0545

    best sale design

---
+++ resources/views/frontend/includes/homepage/advertise.blade.php
@@ -0,0 +1,5 @@
+<section class="advertise">
+    <a href="#">
+        <img src="{{asset('frontend/img/advertises.png')}}" class="w-100" alt="">
+    </a>
+</section>
--- resources/views/frontend/includes/homepage/arrival.blade.php
+++ resources/views/frontend/includes/homepage/arrival.blade.php
@@ -7,44 +7,81 @@
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="arival__block">
                     <img src="{{asset('frontend/img/slider/banner-m.webp')}}" alt="">
-                    <div class="arival-head">
-                        <h6>Earphone</h6>
-                    </div>
                     <div class="shop">
-                        <a href="lisiting.php" class="site-button">Enter Shop</a>
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="arival__block">
                     <img src="{{asset('frontend/img/slider/b-1.webp')}}" alt="">
-                    <div class="arival-head">
-                        <h6>Earphone</h6>
-                    </div>
                     <div class="shop">
-                        <a href="lisiting.php" class="site-button">Enter Shop</a>
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="arival__block">
                     <img src="{{asset('frontend/img/slider/top-bnnr.webp')}}" alt="">
-                    <div class="arival-head">
-                        <h6>Earphone</h6>
-                    </div>
                     <div class="shop">
-                        <a href="lisiting.php" class="site-button">Enter Shop</a>
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="arival__block">
                     <img src="{{asset('frontend/img/slider/slider-1.jpg')}}" alt="">
-                    <div class="arival-head">
-                        <h6>Earphone</h6>
-                    </div>
                     <div class="shop">
-                        <a href="lisiting.php" class="site-button">Enter Shop</a>
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="arival__block">
+                    <img src="{{asset('frontend/img/slider/slider-2.jpg')}}" alt="">
+                    <div class="shop">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="arival__block">
+                    <img src="{{asset('frontend/img/slider/slider.jpg')}}" alt="">
+                    <div class="shop">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="arival__block">
+                    <img src="{{asset('frontend/img/slider/banner-m.webp')}}" alt="">
+                    <div class="shop">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="arival__block">
+                    <img src="{{asset('frontend/img/slider/b-1.webp')}}" alt="">
+                    <div class="shop">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="arival__block">
+                    <img src="{{asset('frontend/img/slider/top-bnnr.webp')}}" alt="">
+                    <div class="shop">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis morbi interdum ac faucibus augue dui.</h6>
+                        <a href="lisiting.php" class="site-button">view product</a>
                     </div>
                 </div>
             </div>
