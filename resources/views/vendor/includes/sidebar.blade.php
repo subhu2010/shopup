cc4c9bf34ebdde1647595406801fad1920605e8f
subhu2010 <maharjansuzan@gmail.com>
2022-04-21 10:00:09 +0545

    vedor dashboard pages added

--- resources/views/vendor/includes/sidebar.blade.php
+++ resources/views/vendor/includes/sidebar.blade.php
@@ -4,15 +4,15 @@
             <a href="#" class="nav_logo"><img src="img/logo/logo.png" alt="" /> <span class="nav_name">Luxury Seller</span> </a>
             <ul class="nav_list" id="sidebar">
                 <li>
-                    <a href="index.php" class="nav_link active"> <i class="bx bx-grid-alt nav_icon"></i> <span
+                    <a href="{{url('index')}}" class="nav_link active"> <i class="bx bx-grid-alt nav_icon"></i> <span
                             class="nav_name">Dashboard</span> </a>
                 </li>
                 <!-- <li>
-                    <a href="user.php" class="nav_link"> <i class='bx bxs-user nav_icon'></i> <span
+                    <a href="{{url('user')}}" class="nav_link"> <i class='bx bxs-user nav_icon'></i> <span
                             class="nav_name">Profile Overview</span> </a>
                 </li> -->
                 <!-- <li>
-                    <a href="user.php" class="nav_link"> <i class='bx bxs-package'></i> <span class="nav_name">My
+                    <a href="{{url('user')}}" class="nav_link"> <i class='bx bxs-package'></i> <span class="nav_name">My
                             Products</span> </a>
                 </li> -->
 
@@ -23,27 +23,21 @@
                         <i class="bx bxs-chevron-down arrow"></i>
                     </div>
                     <ul class="sub-menu">
-                        <li><a href="manageProduct.php">Manage Products</a></li>
-                        <li><a href="addProduct.php">Add Products</a></li>
-                        <li><a href="manageImage.php">Manage Image</a></li>
+                        <li><a href="{{url('manageProduct')}}">Manage Products</a></li>
+                        <li><a href="{{url('addProduct')}}">Add Products</a></li>
+                        <li><a href="{{url('manageImage')}}">Manage Image</a></li>
                     </ul>
                 </li>
-
-             
                 <li>
-                    <a href="user.php" class="nav_link"> <i class='bx bx-heart'></i> <span
-                            class="nav_name">wishlist</span> </a>
-                </li>
-                <li>
-                    <a href="user.php" class="nav_link"> <i class='bx bx-box'></i> <span class="nav_name">Solo
+                    <a href="{{url('user')}}" class="nav_link"> <i class='bx bx-box'></i> <span class="nav_name">Solo
                             Products</span> </a>
                 </li>
                 <li>
-                    <a href="hustory.php" class="nav_link"> <i class='bx bx-history'></i> <span
+                    <a href="{{url('hustory')}}" class="nav_link"> <i class='bx bx-history'></i> <span
                             class="nav_name">History</span> </a>
                 </li>
                 <li>
-                    <a href="hustory.php" class="nav_link"> <i class='bx bxs-timer'></i> <span class="nav_name">Pending
+                    <a href="{{url('hustory')}}" class="nav_link"> <i class='bx bxs-timer'></i> <span class="nav_name">Pending
                             Details</span> </a>
                 </li>
 
@@ -53,8 +47,8 @@
                         <i class="bx bxs-chevron-down arrow"></i>
                     </div>
                     <ul class="sub-menu">
-                        <li><a href="order.php">Manage Order</a></li>
-                        <li><a href="reviewManage.php">Manage Review</a></li>
+                        <li><a href="{{url('order')}}">Manage Order</a></li>
+                        <li><a href="{{url('reviewManage')}}">Manage Review</a></li>
                     </ul>
                 </li>
 
@@ -64,7 +58,7 @@
                         <i class="bx bxs-chevron-down arrow"></i>
                     </div>
                     <ul class="sub-menu">
-                        <li><a href="contact.php">Contact us</a></li>
+                        <li><a href="{{url('contact')}}">Contact us</a></li>
                     </ul>
                 </li>
 
@@ -75,10 +69,10 @@
                         <i class="bx bxs-chevron-down arrow"></i>
                     </div>
                     <ul class="sub-menu">
-                        <li><a href="profile.php">Profile</a></li>
-                        <li><a href="userManage.php">User Management</a></li>
-                        <li><a href="account.php">Account Setting</a></li>
-                        <!-- <li><a href="chat.php">Chat Setting</a></li> -->
+                        <li><a href="{{url('profile')}}">Profile</a></li>
+                        <li><a href="{{url('userManage')}}">User Management</a></li>
+                        <li><a href="{{url('account')}}">Account Setting</a></li>
+                        <!-- <li><a href="{{url('chat')}}">Chat Setting</a></li> -->
                     </ul>
                 </li>
             </ul>
